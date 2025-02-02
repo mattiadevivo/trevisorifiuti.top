4de37dadb8b652cb5519ba783b903b5786c916cd
Mattia De Vivo <mattia.devivo@igenius.ai>
2025-02-02 13:54:48 +0100

    remove: unuseful stuff

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
 # TVTrashTomorrow
 
 The application reminds you the garbage type that is being collected in your town, in the Treviso area (Italy).
-
-## Run the project
-
-Start [ollama](https://ollama.com/) using Docker:
-
-```shell
-docker compose up -d
-```
-
-Download the model you prefer from the ollama library using the command
-
-```shell
-docker exec ollama ollama run <model-name>
-```
-
-If you want to remove a model you can use the command:
-
-```shell
-docker exec ollama ollama rm <model-name>
-```
--- scrape.py
+++ scrape.py
@@ -1,27 +1,44 @@
-import json
-from scrapegraphai.graphs import SmartScraperGraph
+import httpx
+from bs4 import BeautifulSoup, Tag
+from typing import cast
 
-graph_config = {
-    "llm": {
-        "model": "ollama/llama3.2:3b",
-        "temperature": 0,
-        "format": "json",  # Ollama needs the format to be specified explicitly
-        "base_url": "http://localhost:11434",  # set Ollama URL
-    },
-    # "embeddings": {
-    #    "model": "ollama/nomic-embed-text",
-    #    "base_url": "http://localhost:11434",  # set Ollama URL
-    # },
-    "verbose": True,
-    "headless": False,
-}
+# Send a GET request to the webpage
+url = "https://contarina.it/cittadino/raccolta-differenziata/eco-calendario"
 
-smart_scraper_graph = SmartScraperGraph(
-    prompt='Extract the list of "Svuotamenti" for each "Comune", day by day. You can find those information in the <table> tags with class that follow this pattern “table comune comune_zona_<number>" where <number> is the label assigned to the "Comune"',
-    source="https://contarina.it/cittadino/raccolta-differenziata/eco-calendario",
-    config=graph_config,
-)
 
-result = smart_scraper_graph.run()
+def get_page_content(url) -> bytes:
+    response = httpx.get(url)
+    if response.status_code != 200:
+        raise (Exception)
+    return response.content
 
-print(json.dumps(result, indent=4))
+
+def scrape(url: str):
+    page_content = get_page_content(url)
+    # Create a BeautifulSoup object and specify the parser
+    soup = BeautifulSoup(page_content, "html.parser")
+    # Find all table elements with class that follow the pattern "table comune comune_zona_<number>"
+    tables = soup.find_all(
+        attrs={"class": lambda x: x and x.startswith("table svuotamenti")}
+    )
+    for table in tables:
+        table = cast(Tag, table)
+        print(table)
+        print(f"Comune: {table.find('th', string=True).text.strip()}")
+        # Find all rows in the table
+        rows = table.find_all("tr")
+        for row in rows[1:]:
+            cols = row.find_all("td")
+            if len(cols) > 0:
+                comune = cols[0].find("span", string=True).text.strip()
+                data = cols[1].find("span", string=True).text.strip()
+                svuotamenti = cols[2].find_all("span", string=True)
+                # Print each day's waste collection point
+                for i, svu in enumerate(svuotamenti):
+                    print(f"  {data}:")
+                    print(f"    {comune} ({svu.text.strip()})")
+    else:
+        print("Failed to retrieve the webpage")
+
+
+scrape(url)
